1ce2fe7d50981bb9c78072505ce38a31aea4f5d7
Mike Fullerton <mike@greentongue.com>
2013-08-10 20:07:58 -0700

    move up one

---
+++ FLTilingPhotoViewController.h
@@ -0,0 +1,14 @@
+//
+//  FLTilingPhotoViewController.h
+//  FishLamp-iOS-Lib
+//
+//  Created by Mike Fullerton on 1/20/12.
+//  Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton. 
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FLViewController.h"
+
+@interface FLTilingPhotoViewController : FLViewController
+
+@end
---
+++ FLTilingPhotoViewController.m
@@ -0,0 +1,14 @@
+//
+//  FLTilingPhotoViewController.m
+//  FishLamp-iOS-Lib
+//
+//  Created by Mike Fullerton on 1/20/12.
+//  Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton. 
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FLTilingPhotoViewController.h"
+
+@implementation FLTilingPhotoViewController
+
+@end
